710a81b8edb737b3e2e88f9e5e6ef84cacd7d092
yzb1000 <yzb1000@163.com>
2015-04-14 16:16:10 +0800

    m1
    n1

--- sqliteviewer/Form1.cs
+++ sqliteviewer/Form1.cs
@@ -27,7 +27,7 @@ namespace sqliteviewer
             string sql ;
             SQLiteCommand command ;
             
-
+            //16:15
             sql = "select name from sqlite_master where type='table' order by name";
             command = new SQLiteCommand(sql, m_dbConnection);
             SQLiteDataReader reader = command.ExecuteReader();
